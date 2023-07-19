da24630bbc880c9d22653fb9d8b63d0b6aa95781
Tawer95 <73060710+Tawer95@users.noreply.github.com>
2023-07-19 18:08:33 +0500

    Create chains_settings.py

---
+++ settings_merkle/chains_settings.py
@@ -0,0 +1,88 @@
+DATA_CHAINS = {
+    'nova': {
+        'contract': '0x484c402B0c8254BD555B68827239BAcE7F491023',
+        'chain_id': 175,
+        'endpoint': '0x4EE2F9B7cf3A68966c370F3eb2C16613d3235245',
+        'min_balance': 0.001,
+        'native': 'ETH',
+    },
+    'polygon': {
+        'contract': '0xa184998eC58dc1dA77a1F9f1e361541257A50CF4',
+        'chain_id': 109,
+        'endpoint': '0x3c2269811836af69497E5F486A85D7316753cf62',
+        'min_balance': 2,
+        'native': 'MATIC',
+        # 'USDC': '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174',
+        # 'USDT': '0xc2132D05D31c914a87C6611C10748AEb04B58e8F',
+    },
+    'polygon_zk': {
+        'contract': '0xb58f5110855fBEF7A715d325D60543E7D4c18143',
+        'chain_id': 158,
+        'endpoint': '0x9740FF91F1985D8d2B71494aE1A2f723bb3Ed9E4',
+        'min_balance': 2,
+        'native': 'MATIC',
+        # 'USDC': '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174',
+        # 'USDT': '0xc2132D05D31c914a87C6611C10748AEb04B58e8F',
+    },
+    'arbitrum': {
+        'contract': '0xAa58e77238f0E4A565343a89A79b4aDDD744d649',
+        'chain_id': 110,
+        'endpoint': '0x3c2269811836af69497E5F486A85D7316753cf62',
+        'min_balance': 0.001,
+        'native': 'ETH',
+        # 'USDC': '0xff970a61a04b1ca14834a43f5de4533ebddb5cc8',
+        # 'USDT': '0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9',
+    },
+    'optimism': {
+        'contract': '0xa2C203d7EF78ed80810da8404090f926d67Cd892',
+        'chain_id': 111,
+        'endpoint': '0x3c2269811836af69497E5F486A85D7316753cf62',
+        'min_balance': 0.001,
+        'native': 'ETH',
+        # 'USDC': '0x7f5c764cbc14f9669b88837ca1490cca17c31607',
+    },
+    'bsc': {
+        'contract': '0xFDc9018aF0E37AbF89233554C937eB5068127080',
+        'chain_id': 102,
+        'endpoint': '0x3c2269811836af69497E5F486A85D7316753cf62',
+        'min_balance': 0.008,
+        'native': 'BNB',
+        # 'USDT': '0x55d398326f99059ff775485246999027b3197955',
+        # 'BUSD': '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56',
+
+    },
+    'fantom': {
+        'contract': '0x97337A9710BEB17b8D77cA9175dEFBA5e9AFE62e',
+        'chain_id': 112,
+        'endpoint': '0xb6319cC6c8c27A8F5dAF0dD3DF91EA35C4720dd7',
+        'min_balance': 6,
+        'native': 'FTM',
+        # 'USDC': '0x04068DA6C83AFCFA0e13ba15A6696662335D5B75',
+
+    },
+    'avalanche': {
+        'contract': '0xE030543b943bdCd6559711Ec8d344389C66e1D56',
+        'chain_id': 106,
+        'endpoint': '0x3c2269811836af69497E5F486A85D7316753cf62',
+        'min_balance': 0.06,
+        'native': 'AVAX',
+        # 'USDC': '0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E',
+        # 'USDT': '0x9702230a8ea53601f5cd2dc00fdbc13d4df4a8c7',
+    },
+    'zksync': {
+        'contract': '0x6dd28C2c5B91DD63b4d4E78EcAC7139878371768',
+        'chain_id': 165,
+        'endpoint': '0x9b896c0e23220469C7AE69cb4BbAE391eAa4C8da',
+        'min_balance': 0.001,
+        'native': 'ETH',
+        # 'USDC': '0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E',
+        # 'USDT': '0x9702230a8ea53601f5cd2dc00fdbc13d4df4a8c7',
+    },
+}
+text = '''  #####                                          #####                                
+ #     # #####  #   # #####  #####  ####        #     # ###### #      #    # # ###### 
+ #       #    #  # #  #    #   #   #    #       #       #      #      #   #  # #      
+ #       #    #   #   #    #   #   #    # #####  #####  #####  #      ####   # #####  
+ #       #####    #   #####    #   #    #             # #      #      #  #   # #      
+ #     # #   #    #   #        #   #    #       #     # #      #      #   #  # #      
+  #####  #    #   #   #        #    ####         #####  ###### ###### #    # # ###### '''
